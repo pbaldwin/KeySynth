85b22fb15180df8e2de7ad807a75ba1c335e52db
Partrick Developer <partrickdeveloper@Patricks-MBP.click-network.com>
2017-03-05 14:51:17 -0800

    Use consts in scale maker test spec.

--- test/unit/scale-maker.spec.js
+++ test/unit/scale-maker.spec.js
@@ -1,8 +1,9 @@
 import scaleMaker from '../../src/utils/scale-maker'
+import Consts from '../../src/utils/consts'
 
 describe('scale-maker', () => {
-  const chromaticScale = [...Array(12)].map(() => 1)
-  const tonic = 261.6
+  const chromaticScale = Consts.modes.chromatic
+  const tonic = Consts.tonics['c']
   const comparison = [ 261.6, 277.2, 293.6, 311.1, 329.6, 349.2, 370, 392, 415.3, 440, 466.1, 493.8, 523.2 ]
 
   it('should return chromatic scale from middle C', () => {
